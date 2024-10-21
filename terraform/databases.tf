c22bb89c979a2749d788c5aac24b2c3f49ae806d
yassinedehbi <yassinedehbi4@gmail.com>
2024-10-21 16:26:59 +0200

    add schemas depre

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -82,7 +82,7 @@ resource "snowflake_grant_privileges_to_account_role" "schema_grant3" {
 
 resource "snowflake_grant_privileges_to_account_role" "tables_grant" {
   privileges        = ["SELECT", "INSERT"]
-  account_role_name = snowflake_account_role.dev_role.name
+  account_role_name = snowflake_role.dev_role.name
   on_schema_object {
     future {
       object_type_plural = "TABLES"
