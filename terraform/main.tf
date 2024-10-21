3b1cfa21e8ad07dce946f8565dd745229c825d76
yassinedehbi <yassinedehbi4@gmail.com>
2024-10-21 15:51:52 +0200

    no validate 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
     }
   }
 
