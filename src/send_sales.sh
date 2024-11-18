8566364415b704f069c614b496a557e4c89d395d
eddaouissam <eddaouissam@gmail.com>
2024-11-18 16:35:53 +0100

    Add src folder [skip actions]

---
+++ src/send_sales.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define source path and stage path
+source_path="Place path to files here"
+stage_path="@SALES_DATA"
+
+# Ensure the stage path ends with a slash
+stage_path="${stage_path%/}/"
+
+# Loop through each file present in the source path
+for filepath in "$source_path"/*; do
+    # Get the base name of the file
+    filename=$(basename "$filepath")
+
+    # Construct the target path in the stage
+    target_path="$stage_path"
+
+    # Execute the PUT command to load the files into Snowflake
+    echo "PUT file://$filepath $target_path;"
+    snowsql -q "PUT file://$filepath $target_path;"
+done
